f25491d4eaaf7d6b0946c8ce61df352ade2beaae
Angus McMorland <amcmorl@gmail.com>
2008-08-07 18:19:10 -0400

    Adding multiregress, which calculates multiple regression statistics

---
+++ multiregress.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def multiregress(xs, y):
+    '''Multiple linear regression with statistics.
+    
+    Calculates the partial regression co-efficients b_{1..k} in the
+    equation Y = b_0 + b.x_1 + b.x_2 + ... + b.x_k
+
+    Returns the standard errors of the regression coefficients:
+    s_{bY1}, s_{bY2}, as described in Biometry, Sokal and Rohlf, 3rd Ed.,
+    box 16.2.
+
+    Parameters
+    ----------
+    xs : array, shape (n, k)
+        k is number of variates, n is number of observations
+    y : array, shape (n,)
+
+    Returns
+    -------
+    b : array, shape (k + 1)
+        partial regression co-efficients, first element is constant term
+    errs : array, shape (k + 1)
+        standard errors of regression co-efficients in b
+    '''
+    # add ones for linear regression
+    n, k = xs.shape
+    xs_c = np.concatenate((np.ones((n,1)), xs), axis=1)
+    b, resid, rank, s = np.linalg.lstsq(xs_c, y)
+    s_y = np.sqrt(resid / (n - k - 1.)) # unexplained SD
+    G_mult = np.linalg.inv( np.cov( xs.T ) * (n - 1) )
+    errs = s_y * np.sqrt(G_mult[np.eye(k, dtype=bool)])
+    return b, errs
+
